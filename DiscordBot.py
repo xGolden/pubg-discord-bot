9f6226bd26aaab536baaed3095d0bbca08ddeb60
xGolden <brownchrisg@gmail.com>
2018-07-10 12:45:48 -0400

    Add files via upload

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,59 +1,88 @@
 import discord
 import config
-import requests
+import json
 
 client = discord.Client()
 
-regulars = []
+def read_regulars():
+    with open('regulars.json', 'r') as infile:
+        data = json.load(infile)
+        print(data)
+    print(data)
+    return data
+
+def write_regulars():
+    print("Writing to regulars.json")
+    with open('regulars.json', 'w') as outfile:
+        json.dump(regulars, outfile, sort_keys = True, indent = 4, ensure_ascii = False)
+
+regulars = read_regulars()
 
 @client.event
 async def on_message(message):
     #the bot should not reply to itself
     if message.author != client.user and message.content.startswith(config.COMMANDPREFIX):
         msg = message.content.split(" ")
-        cmd = msg[0][1:].lower()
-        
-        #responds with a link to the pubg.op.gg profile of the indicated player
+        cmd = msg.pop(0)[1:].lower()
+
+        #No parameters - looks up message owner in the regulars list and provides link
+        #Paramaters can be used to look up any player
         if cmd == "stats":
-            link = "https://pubg.op.gg/user/" + msg[1] + "?server=pc-na"
-            await client.send_message(message.channel, link)
-    
+            print("Displaying stats")
+            prefix = "https://pubg.op.gg/user/"
+            suffix = "?server=pc-na"
+            if msg:
+                for name in msg:
+                    await client.send_message(message.channel, prefix + msg + suffix)
+            elif message.author.id in regulars:
+                await client.send_message(message.channel, prefix + regulars[message.author.id] + suffix)
+        
+        #Format command as '!addme <PUBG NAME>', adds message owner to the regulars list
+        elif cmd == "addme" and msg:
+            if message.author.id not in regulars:
+                print("Added " + message.author.id + " to the list of regulars as " + msg[0])
+                regulars[message.author.id] = msg[0]
+                await client.send_message(message.channel, "Added " + message.author.id + " to the list of regulars as " + msg[0])
+
+        elif cmd == "removeme":
+            print("Removing " + message.author.id + " from list of regulars as " + regulars[message.author.id])
+            del regulars[message.author.id]
+            
         #responds with a link to the list of 2d match telemetry of the indicated player
         elif cmd == "2d":
-            link = "https://pubg.sh/" + msg[1] + "/pc-na"
-            await client.send_message(message.channel, link)
-        
-        #shutsdown the bot - only usable by the bot owner specified in the config
+            prefix = "https://pubg.sh/"
+            suffix = "/pc-na"
+            if msg:
+                for name in msg:
+                    await client.send_message(message.channel, prefix + msg + suffix)
+            elif message.author.id in regulars:
+                await client.send_message(message.channel, prefix + regulars[message.author.id] + suffix)
+
+        #shutsdown the bot and saves list of regulars - only usable by the bot owner specified in the config
         elif cmd == "shutdown":
             if message.author.id == config.OWNERID:
-                await client.send_message(message.channel, "Shutting down...")
+                await client.send_message(message.channel, "Saving list of regulars and shutting down...")
+                write_regulars()
                 await client.logout()
             else:
                 await client.send_message(message.channel, "You do not have permission to use this command.")
-    
-        #adds a name to the list of regulars to query the API
-        elif cmd == "add":
-            await client.send_message(message.channel, "Adding " + msg[1] + " to list of regulars.")
-            regulars.append(msg[1])
-        
-        #removes a name from the list of regulars
-        elif cmd == "remove":
-            await client.send_message(message.channel, "Removing " + msg[1] + " from list of regulars.")
-            regulars.remove(msg[1])
-        
-        #prints a list of the regular players
-        elif cmd == "regulars":
-            if not regulars:
-                await client.send_message(message.channel, "List of regulars is empty.")
-            else:
-                await client.send_message(message.channel, "The current list of regulars is: " + ', '.join(regulars))
 
+        #Prints out a list of regular players
+        elif cmd == "regulars":
+            if regulars:
+                for key,value in regulars.items():
+                    await client.send_message(message.channel, key + " saved as " + value)
+            else:
+                print("Regulars Dictionary is empty")
 @client.event
 async def on_ready():
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
     print("--------")
+    #regulars = read_regulars()
+    #print(regulars)
+    print("Reading from regulars.json")
 
 
 client.run(config.TOKEN)
