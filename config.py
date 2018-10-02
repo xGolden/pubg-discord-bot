3d755ba5b4c612d324ed18521600d37ba030f5fa
xGolden <brownchrisg@gmail.com>
2018-10-02 13:24:13 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+TOKEN = ""
+APIKEY = ""
+OWNERID = ""
+COMMANDPREFIX = "!"
+
+HELPFILE = """```!addme <PUBG NAME> : adds you to the list of players
+
+!removeme : removes you from the list of players
+
+!pubg.op <Optional list of PUBG names> : with no parameter, will return a link to your pubg.op.gg page, otherwise will link to the players provided
+
+!2d <Optional list of PUBG names> : with no parameter, will return a link to your pubg.sh page, otherwise will link to the players provided
+
+!sh <Optional list of PUBG names> : alternate command to above
+
+!regulars : provides a list of the saved players
+
+!shutdown : owner only command, shuts the bots down
+```"""
